e717b275025e93db732bb3187e3ee6141c5df286
ThammanoonAMON <42894739+ThammanoonAMON@users.noreply.github.com>
2018-12-18 22:47:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ restService.use(bodyParser.json());
 restService.post("/echo", function(req, res) {
   var speech = output;
   return res.json({
-    text: speech,
+    speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
   });
