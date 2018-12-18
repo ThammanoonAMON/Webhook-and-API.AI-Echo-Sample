3e4dffb4013e5213afcb1ede5be2f65109d2ae9e
ThammanoonAMON <42894739+ThammanoonAMON@users.noreply.github.com>
2018-12-18 21:31:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const cheerio = require("cheerio");
 
 const restService = express();
 
@@ -15,32 +13,24 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-request("https://www.sanook.com/horoscope/152061/", (error, response, html) => {
-  if (err) {
-    reject(err);
-    return console.log("Failed to request: ", err);
-  }
-
-  const $ = cheerio.load(html);
-  const luck = $(".jsx-2224007166 .jsx-3435773413");
-  const output = luck
-    .find("p")
-    .slice(0, 3)
-    .text();
-  restService.post("/echo", function(req, res) {
-    var speech = output;
-    return res.json({
-      speech: "speech",
-      displayText: "speech",
-      source: "webhook-echo-sample"
-    });
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
   });
 });
 
 restService.post("/audio", function(req, res) {
   var speech = "";
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language
+    //Speech Synthesis Markup Language 
     case "music one":
       speech =
         '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
