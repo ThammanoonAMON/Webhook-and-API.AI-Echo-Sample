e195059b023706fd4badc96f6af51c3d81eb1ba5
ThammanoonAMON <42894739+ThammanoonAMON@users.noreply.github.com>
2018-12-19 01:08:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,84 +21,86 @@ restService.use(
     extended: true
   })
 );
-
-request("https://www.sanook.com/horoscope/152061/", (error, response, html) => {
-  if (!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-
-    const luck = $(".jsx-2224007166 .jsx-3435773413");
-    output1 = luck
-      .find("p")
-      .slice(0, 1)
-      .text();
-
-    output2 = luck
-      .find("p")
-      .slice(1, 2)
-      .text();
-
-    output3 = luck
-      .find("p")
-      .slice(2, 3)
-      .text();
-
-    output4 = luck
-      .find("p")
-      .slice(3, 4)
-      .text();
-
-    output5 = luck
-      .find("p")
-      .slice(4, 5)
-      .text();
-
-    output6 = luck
-      .find("p")
-      .slice(5, 6)
-      .text();
-
-    output7 = luck
-      .find("p")
-      .slice(6, 7)
-      .text();
-
-    output8 = luck
-      .find("p")
-      .slice(7, 8)
-      .text();
-
-    result =
-      "ดวงคนเกิดวันจัทร์ วันนี้ :" +
-      "\n" +
-      "\n" +
-      output1 +
-      "\n" +
-      "\n" +
-      output2 +
-      "\n" +
-      "\n" +
-      output3 +
-      "\n" +
-      "\n" +
-      output4 +
-      "\n" +
-      "\n" +
-      output5 +
-      "\n" +
-      "\n" +
-      output6 +
-      "\n" +
-      "\n" +
-      output7 +
-      "\n" +
-      "\n" +
-      output8;
-  }
-});
-
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
+  request(
+    "https://www.sanook.com/horoscope/152061/",
+    (error, response, html) => {
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        const luck = $(".jsx-2224007166 .jsx-3435773413");
+        output1 = luck
+          .find("p")
+          .slice(0, 1)
+          .text();
+
+        output2 = luck
+          .find("p")
+          .slice(1, 2)
+          .text();
+
+        output3 = luck
+          .find("p")
+          .slice(2, 3)
+          .text();
+
+        output4 = luck
+          .find("p")
+          .slice(3, 4)
+          .text();
+
+        output5 = luck
+          .find("p")
+          .slice(4, 5)
+          .text();
+
+        output6 = luck
+          .find("p")
+          .slice(5, 6)
+          .text();
+
+        output7 = luck
+          .find("p")
+          .slice(6, 7)
+          .text();
+
+        output8 = luck
+          .find("p")
+          .slice(7, 8)
+          .text();
+
+        result =
+          "ดวงคนเกิดวันจัทร์ วันนี้ :" +
+          "\n" +
+          "\n" +
+          output1 +
+          "\n" +
+          "\n" +
+          output2 +
+          "\n" +
+          "\n" +
+          output3 +
+          "\n" +
+          "\n" +
+          output4 +
+          "\n" +
+          "\n" +
+          output5 +
+          "\n" +
+          "\n" +
+          output6 +
+          "\n" +
+          "\n" +
+          output7 +
+          "\n" +
+          "\n" +
+          output8;
+      }
+    }
+  );
+
   return res.json({
     fulfillmentText: result
   });
